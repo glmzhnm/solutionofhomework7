9505196c683afc520415ef7b9fffad6e21d1da9e
glmzhnm <galymzhnmbazarbayev@gmail.com>
2025-04-24 19:07:03 +0500

    season class

--- src/Season.java
+++ src/Season.java
@@ -1,2 +1,19 @@
-public class Season {
+import java.util.*;
+public class Season implements Iterable<Episode> {
+    private List<Episode> episodes;
+
+    public Season() {
+        episodes = new ArrayList<>();
+    }
+
+    public void addEpisode(Episode e) {
+        episodes.add(e);
+    }
+    public List<Episode> getEpisodes() {
+        return episodes;
+    }
+    @Override
+    public Iterator<Episode> iterator() {
+        return new SeasonIterator(episodes);
+    }
 }
